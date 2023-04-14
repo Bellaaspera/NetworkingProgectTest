9ddf2be7de18cdf3f6ffefe4ec3f5dffce9a6534
Bellaaspera <lanasenatorova@gmail.com>
2023-04-14 16:55:51 +0300

    Initial Commit

---
+++ NetworkingProgectTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NetworkingProgectTest
+//
+//  Created by Светлана Сенаторова on 14.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
